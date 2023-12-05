fccbd3483ee3a83723353b13ca90f7a7051d761c
salamandercrossing <giulio.zani@gmail.com>
2023-12-05 15:30:35 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 import { useChatSession } from "@chainlit/react-client";
 import { Playground } from "./components/playground";
 
-const CHAINLIT_SERVER = "http://localhost:8000";
+//const CHAINLIT_SERVER = "http://localhost:8000";
+const CHAINLIT_SERVER = "https://aikosmos-rkaflb3bpq-ew.a.run.app";
 const userEnv = {};
 
 function App() {
